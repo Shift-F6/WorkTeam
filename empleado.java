b054ae3cfbf2d7f66c0e964b0ae9d73f2e4e80d0
Rafael Bautista <nuevoid00@gmail.com>
2015-06-26 15:32:48 -0500

    aqui esta mi parte

--- empleado.java
+++ empleado.java
@@ -5,8 +5,22 @@ public class empleado {
 	//Ivan metodos
 	//Pedro main
 	//Ivan Coments
+	public String cveEmpleado;
+	public String nombre;
+	public String apellido;
+	public String cargo;
+	public double sueldo;
+
 	public void empleado() {
 		
 		
 	}
+		public void empleado(String cveEmpleado, String nombre, String apellido, String cargo, double sueldo){
+			this.cveEmpleado = cveEmpleado;
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.cargo = cargo;
+			this.sueldo = sueldo;
+		}
+
 }
